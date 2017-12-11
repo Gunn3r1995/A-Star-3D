56bdb80b00ea110ab04b97f21661807f4a9bf2b8
Gunn3r1995 <smithateden@hotmail.com>
2017-12-11 19:25:47 +0000

    Initial commit

---
+++ Assets/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    public bool walkable;
+    public Vector3 WorldPosition;
+
+    public Node(bool _walkable, Vector3 _worldPosition)
+    {
+        walkable = _walkable;
+        WorldPosition = _worldPosition;
+    }
+}
