c5be7b05fbb3e9d8c642ddb880cbcad144bdd8d0
Shane Smith <srs23@brighton.ac.uk>
2017-12-11 20:53:50 +0000

    implementing A* pathfinding

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Node {
+        public bool walkable;
+        public Vector3 WorldPosition;
+        public int gridX;
+        public int gridY;
+
+        public int GCost;
+        public int HCost;
+        public int FCost
+        {
+            get { return GCost + HCost; }
+        }
+
+        public Node Parent;
+
+        public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY)
+        {
+            walkable = _walkable;
+            WorldPosition = _worldPosition;
+            gridX = _gridX;
+            gridY = _gridY;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Pathfinding : MonoBehaviour
+    {
+        public Transform Seeker, Target;
+
+        private Grid grid;
+
+        private void Awake()
+        {
+            grid = GetComponent<Grid>();
+        }
+
+        private void Update()
+        {
+            FindPath(Seeker.position, Target.position);
+        }
+
+        private void FindPath(Vector3 startPos, Vector3 targetPos)
+        {
+            Node startNode = grid.GetNodeFromWorldPoint(startPos);
+            Node targetNode = grid.GetNodeFromWorldPoint(targetPos);
+
+            List<Node> openSet = new List<Node>();
+            HashSet<Node> closedSet = new HashSet<Node>();
+
+            openSet.Add(startNode);
+
+            while (openSet.Count > 0)
+            {
+                Node currentNode = openSet[0];
+                for (int i = 1; i < openSet.Count; i++)
+                {
+                    if (openSet[i].FCost <= currentNode.FCost && openSet[i].HCost < currentNode.HCost)
+                    {
+                        currentNode = openSet[i];
+                    }
+                }
+
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode)
+                {
+                    RetracePath(startNode, targetNode);
+                    return;
+                }
+
+                foreach (Node neighbour in grid.GetNeighbours(currentNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbour = currentNode.GCost + GetDistance(currentNode, neighbour);
+                    if (newMovementCostToNeighbour < neighbour.GCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.GCost = newMovementCostToNeighbour;
+                        neighbour.HCost = GetDistance(neighbour, targetNode);
+                        neighbour.Parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RetracePath(Node startNode, Node targetNode)
+        {
+            List<Node> path = new List<Node>();
+            Node currentNode = targetNode;
+
+            while (currentNode != startNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.Parent;
+            }
+            path.Reverse();
+
+            grid.Path = path;
+        }
+
+        private int GetDistance(Node nodeA, Node nodeB)
+        {
+            int distanceX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+            int distanceY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+            if (distanceX > distanceY)
+            {
+                return 14 * distanceY + 10 * (distanceX - distanceY);
+            }
+            return 14 * distanceX + 10 * (distanceY - distanceX);
+        }
+    }
+}
